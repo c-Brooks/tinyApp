222793955caf64a67aed5d195d104d22ca8b8926
vagrant <vagrant@lighthouse-labs>
2016-09-17 02:36:40 +0000

    Switching to MongoDB database

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const app = express();
-const PORT = process.env.PORT || 8080; // default port 8080
 
+const PORT = process.env.PORT || 8080; // default port 8080
+const database = require('./database.js')
 app.set('view engine', 'ejs');
 
 app.use(express.static('views'));
@@ -21,6 +22,25 @@ var urlDatabase = {
 app.use(methodOverride('_method'));
 
 
+const MongoClient = require("mongodb").MongoClient;
+const MONGODB_URI = "mongodb://127.0.0.1:27017/url_shortener";
+console.log(`Connecting to MongoDB running at: ${MONGODB_URI}`);
+
+
+var db;
+
+
+MongoClient.connect(MONGODB_URI, function(err, database1) {
+  //const collection = database.collection('test');
+  if (err) return console.log(err);
+  db = database1;
+  app.listen(8080, () => {
+    console.log('listening on 8080');
+  });
+});
+
+
+
 // ----------------------------------------------------------- //
 // ----------------------------------------------------------- //
 // ----------------------------------------------------------- //
@@ -29,7 +49,10 @@ app.use(methodOverride('_method'));
 
 // SHOW ALL
 app.get('/urls', (req, res) => {
-  res.render('urls_index', {urls: urlDatabase});
+  db.collection('test').find().toArray( (err, result) => {
+    console.log(result);
+     res.render('urls_index', {urls: result});
+  });
 });
 
 // CREATE NEW
@@ -38,53 +61,51 @@ app.get('/urls/new', (req, res) => {
 });
 
 app.post('/urls/', (req, res) => {
-  urlDatabase[randomString()] = req.body.longURL;
-  res.redirect('/urls')
+var shortURL = randomString();
+  db.collection('test').insert({ shortURL: shortURL, longURL: req.body.longURL },  function(err) {
+    if (err) return res.redirect('/new');
+    res.redirect('/urls');
+  });
 });
 
 // DELETE
 app.delete('/urls/:id', (req, res) => {
-  delete urlDatabase[req.params.id];
+  console.log(req.params.id);
+  db.collection('test').removeOne({shortURL: req.params.id});
   res.redirect('/urls');
   });
 
 
 // PUT
 app.put('/urls/:id', (req, res) => {
-  console.log(req.body)
   urlDatabase[req.params.id] = req.body.longURL;
   res.redirect('/urls')
+
+  db.collection('test').updateOne( {'shortURL': req.params.id},
+    {$set:{'longURL': req.body.longURL}}, (err, result) => {
+    res.render('urls_show', {urls: result[0]});
+  });
 });
 
 
-
 // GO TO URL
-app.get('/u/:shortURL', (req, res) => {
-  const sURL = req.params.shortURL
- if(urlDatabase.hasOwnProperty(sURL)){
-    var longURL = urlDatabase[sURL];
-    res.redirect(longURL);
-  }
-  else res.redirect('/urls')
+app.get('/u/:id', (req, res) => {
+  console.log("id: ", req.params.id)
+  console.log("res body: ", res.body)
+  db.collection('test').find( {'shortURL': req.params.id.shortURL}, (err, result) => {
+   //res.redirect(result)
+   console.log(result);
+  });
 });
 
 // SHOW INDIVIDUAL
 app.get('/urls/:id', (req, res) => {
-  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
-  res.render('urls_show', templateVars);
-});
+  var shortURL = req.params.id;
+  db.collection('test').findOne({'shortURL': shortURL}, (err, result) => {
+    res.render('urls_show', {urls: result});
+    });
+  });
 
-// EDIT
-
-
-
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
 
 
 
@@ -93,3 +114,6 @@ function randomString() {
   return Math.round((Math.pow(baseNum, length-1) - Math.random() *
           Math.pow(baseNum, length))).toString(baseNum).slice(1);
 }
+
+
+
